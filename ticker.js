d32ff79eb23b44198dc63b5bb8caaaef09f060e7
Lakshmi Narayana <narayana.8.dasari@gmail.com>
2018-06-19 11:48:00 -0500

    Update ticker.js

--- ticker.js
+++ ticker.js
@@ -1,21 +1,22 @@
-var speed;
-var frame;
-var obj;
+// script goes here
 
+var request = new XMLHttpRequest();
+var x = "";
+request.open('GET', 'https://wrapapi.com/use/devera2g0/pokernewsdaily/titles/0.0.1?wrapAPIKey=3K63CIKYRlpfuyuoVOcSJgf2tOY5uPxp', true);
+request.onload = function () {
 
-function setup(){
-		createCanvas(window.innerWidth,80);
-		frame = 0;
-		obj = 0;
-		speed = 0;
-}
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    console.log(request.statusText);
+    var myObj = data.data;
+    for(var i in myObj.output){
+      x+= "&#9671;"+myObj.output[i].title;
+    }
+    document.getElementById("news").innerHTML = x;
+  } else {
+    console.log('error');
+  }
+};
 
-function draw(){
-	background(0);
-	speed = speed + .2;
-	fill(200)
-	frame = rect(speed , -1, 100 ,100);
-	//if(frame == end)
-	//frame = (
-	
-}
+request.send();
